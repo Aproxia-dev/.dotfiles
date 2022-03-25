b4098b707042f5edbe02e1da90d4a291b3a00e73
Call-Me-Apro <apro@r4ilax.eu>
2022-03-25 15:46:41 +0100

    Big overhaul

---
+++ .config/eww/scripts/worksp.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+from subprocess import run
+
+
+def workspaces():
+    global wsboxes
+    workspaces = [ "1", "2", "3", "4", "5" ]
+    monitors   = [ "HDMI-A-0", "DisplayPort-2" ]
+
+    wsboxes = ""
+
+    #for monitor in monitors:
+    monitor="HDMI-A-0"
+    for workspace in workspaces:
+        _class = checkws(monitor, workspace)
+        wsboxes += "(button :class '"+_class+"' :onclick 'bspc desktop -f "+workspace+".local' '"+workspace+"')"
+
+def checkws(m, ws):
+    f_cmd = "bspc query -D -d focused -m {} --names".format(m)
+    f_out = run(f_cmd, capture_output=True, text=True, shell=True).stdout.replace('\n','')
+    
+    o_cmd = "bspc query -D -d .occupied -m {} --names".format(m)
+    o_out = run(o_cmd, capture_output=True, text=True, shell=True).stdout.replace('\n','')
+    
+    if ws in f_out: 
+        return "focused"
+    elif ws in o_out:
+        return "occupied"
+    else:
+        return ""
+
+workspaces()
+print("(box :class 'workspaces' :orientation 'v' :halign 'center' :valign 'start' :space-evenly 'false' :spacing '-5'" + wsboxes + " )")
---
+++ .config/nvim/init.vim
@@ -0,0 +1,52 @@
+set nocompatible
+set number relativenumber
+set termguicolors
+
+set wrap!
+
+filetype plugin on
+syntax on
+
+hi Normal guibg=NONE ctermbg=NONE
+
+command WQ wq
+command Wq wq
+command W w
+command Q q
+
+call plug#begin('~/.config/nvim/plugged')
+
+Plug 'catppuccin/nvim', {'as': 'catppuccin'}
+Plug 'ms-jpq/coq_nvim', {'branch': 'coq'}
+Plug 'ms-jpq/coq.artifacts', {'branch': 'artifacts'}
+Plug 'rrethy/vim-hexokinase', { 'do': 'make hexokinase' }
+Plug 'nvim-lualine/lualine.nvim'
+Plug 'kyazdani42/nvim-web-devicons'
+Plug 'romgrk/barbar.nvim'
+Plug 'scrooloose/nerdtree'
+Plug 'elkowar/yuck.vim'
+
+call plug#end()
+
+let g:Hexokinase_highlighters = [ 'backgroundfull' ]
+let g:Hexokinase_optInPatterns = 'full_hex,rgb,rgba,hsl,hsla'
+
+colorscheme catppuccin
+
+lua << EOF
+
+local catppuccin = require("catppuccin")
+
+catppuccin.setup {
+	transparent_background = true,
+	integrations = {
+		barbar = true
+	}
+	
+}
+require('lualine').setup {
+	options = { theme = "catppuccin" }
+}
+
+EOF
+
